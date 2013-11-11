3735f9b1121fa392d13bb4d61613daf303ef9aab
Bennett Siegel <bennett.e.siegel@gmail.com>
2013-11-10 20:00:19 -0500

    Add subdomain support

--- subscribem/spec/features/accounts/sign_up_spec.rb
+++ subscribem/spec/features/accounts/sign_up_spec.rb
@@ -4,6 +4,7 @@ feature 'Accounts' do
 		visit subscribem.root_path
 		click_link 'Account Sign Up'
 		fill_in 'Name', :with => 'Test'
+		fill_in 'Subdomain', :with => 'test'
 		fill_in 'Email', :with => 'subscribem@example.com'
 		fill_in 'Password', :with => 'password', :exact => true
 		fill_in 'Password confirmation', :with => 'password'
@@ -11,5 +12,6 @@ feature 'Accounts' do
 		page.should have_content("Signed in as subscribem@example.com")
 		success_message = 'Your account has been successfully created.'
 		page.should have_content(success_message)
+		page.current_url.should == "http://test.example.com/subscribem/"
 	end
 end
