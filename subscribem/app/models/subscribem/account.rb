811dbf7c926904b83339faded0b423a0f29ca9fc
Bennett Siegel <bennett.e.siegel@gmail.com>
2013-11-10 21:38:22 -0500

    Restrict subdomain names

--- subscribem/app/models/subscribem/account.rb
+++ subscribem/app/models/subscribem/account.rb
@@ -3,5 +3,16 @@ module Subscribem
   	validates :subdomain, :presence => true, :uniqueness => true
   	belongs_to :owner, :class_name => "Subscribem::User"
   	accepts_nested_attributes_for :owner
+
+  	EXCLUDED_SUBDOMAINS = %w(admin)
+  	validates_exclusion_of :subdomain, :in => EXCLUDED_SUBDOMAINS,
+  	:message => "is not allowed. Please choose another subdomain."
+
+  	before_validation do
+  		self.subdomain = subdomain.to_s.downcase
+  	end
+
+  	validates_format_of :subdomain, :with => /\A[\w\-]+\Z/i,
+  		:message => "is not allowed. Please choose another subdomain."
   end
 end
